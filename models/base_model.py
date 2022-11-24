f2486cd481e5fb1d003a187a6be774b142510b29
Precious Idubor <preciousidubor@gmail.com>
2022-11-24 10:17:59 -0800

    +Creates basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,22 @@ class BaseModel:
     """
         Represents the BaseModel class
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             Initialization
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if (len(kwargs) != 0):
+            for k, v in kwargs.items():
+                if (k == 'created_at' or k == 'updated_at'):
+                    self.__dict__[k] = datetime.strptime(v, t_format)
+                else:
+                    if (k != '__class__'):
+                        self.__dict__[k] = v
 
     def __str__(self):
         """
@@ -36,9 +45,10 @@ class BaseModel:
             Returns a dictionary
             containing all keys/values of the instance
         """
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
         in_dct = dict(self.__dict__)
         in_dct['__class__'] = self.__class__.__name__
-        in_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        in_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        in_dct['created_at'] = self.created_at.strftime(t_format)
+        in_dct['updated_at'] = self.updated_at.strftime(t_format)
 
         return (in_dct)
