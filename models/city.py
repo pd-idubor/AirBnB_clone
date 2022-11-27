55718c8e8305aacd6248862e8d282e15b051366a
Precious Idubor <preciousidubor@gmail.com>
2022-11-27 11:56:22 -0800

    More classes - inherit from BaseModel

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Defines 'City' class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+        City class
+    """
+    state_id = ""
+    name = ""
